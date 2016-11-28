ba7404141d7d41b1a157469b9b6c12539c3987be
demiaowu <demiaowu@163.com>
2016-11-28 21:57:58 +0800

    update install.sh: save original configuration

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/sh 
 
 befpwd=$PWD                                                     
-#echo $befpwd
+echo $befpwd
 
-cp -f .vimrc ~
-cp -fr .vim ~
+cd ~
+mkdir -pv .vimtmp
+cd .vimtmp
+rm -vfr *
+cd ..
+
+cp -vf  .vimrc ~/.vimtmp
+cp -vfr .vim   ~/.vimtmp
+
+cd $befpwd
+cp -vf  .vimrc ~
+cp -vfr .vim   ~
+
+echo "安装成功"
+echo "    原先的vim配置文件已经被保存在.vimtmp目录下"
 
 exit 0
